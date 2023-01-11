f31cbadec33571dd75baa838d8cefe078fa99aac
Jolomisan Edema <jolomisan01@gmail.com>
2023-01-11 21:13:10 +0100

    Update car_game.py

--- car_game.py
+++ car_game.py
@@ -1,17 +1,30 @@
-end_command = "quit"
-run_command = input('> ')
-while run_command != end_command:
-    if run_command == "help" or run_command == "HELP":
-        print("start - to start the car")
-        print("stop - to stop the car")
-        print("start - to exit")
-        run_command = input('> ')
-    elif run_command == "START" or run_command == "start":
-        print("Car started... Ready to go" )
-        run_command = input('> ')
-    elif run_command == "STOP" or run_command == "stop":
-        print("Car stopped" )
-        run_command = input('> ')
+command = input('> ')
+is_started = False
+while command != "quit":
+    if command == "help":
+        print('''
+        start - to start the engine
+        stop - to stop the engine
+        quit - to exit   
+        ''')
+    elif command == "start":
+        if is_started:
+            print('car is already started!')
+        else:
+            is_started = True
+            print("Car started... Ready to go")
+
+    elif command == "stop":
+        if not is_started:
+            print('car is already stopped')
+        else:
+            is_started = True
+            print("Car stopped")
+
     else:
         print("I don't understand")
-        run_command = input('> ')
+    command = input('> ')
+
+
+
+
